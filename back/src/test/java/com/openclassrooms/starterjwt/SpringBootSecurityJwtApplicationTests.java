ed8792303ec16a11d2b46a0e19e877d7e867094f
Ali0908 <zaabat97@gmail.com>
2024-09-13 14:59:38 +0200

    feat: BackEnd:  testMain() on SpringBootSecurityJwtApplicationTests progress.

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -2,9 +2,10 @@ package com.openclassrooms.starterjwt;
 
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.SpringApplication;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -13,12 +14,26 @@ public class SpringBootSecurityJwtApplicationTests {
 	private UserRepository userRepository;
 
 	@Test
+	public void contextLoads() {
+	}
+
+	@Test
 	public void testUserRepositoryIsLoaded() {
 		assertNotNull(userRepository);
 	}
 
+
 	@Test
-	public void contextLoads() {
+	public void testMain() {
+		// Mock the SpringApplication.run() method
+		try {
+			Mockito.mockStatic(SpringApplication.class);
+			SpringBootSecurityJwtApplication.main(new String[] {});
+			Mockito.verify(SpringApplication.class, Mockito.times(1));
+		} finally {
+			// Clear mocks to prevent memory leaks
+			Mockito.clearAllCaches();
+		}
 	}
 
 }
