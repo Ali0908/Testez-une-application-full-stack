f3d84d226a943d17ba4a4bc31dace17320224428
Ali0908 <zaabat97@gmail.com>
2024-09-13 14:41:19 +0200

    feat: BackEnd:  testUserRepositoryIsLoaded() on SpringBootSecurityJwtApplicationTests progress.

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,10 +1,21 @@
 package com.openclassrooms.starterjwt;
 
+import com.openclassrooms.starterjwt.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void testUserRepositoryIsLoaded() {
+		assertNotNull(userRepository);
+	}
 
 	@Test
 	public void contextLoads() {
