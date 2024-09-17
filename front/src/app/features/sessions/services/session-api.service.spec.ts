944b281ecae9e7508f3333c5098dbf5fc5b8294a
Ali0908 <zaabat97@gmail.com>
2024-09-17 15:32:04 +0200

    feat: FrontEnd:  88% coverage code done.

--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -9,7 +9,7 @@ describe('SessionsService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
+      imports: [
         HttpClientModule
       ]
     });
@@ -18,5 +18,56 @@ describe('SessionsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Integration test: should return a list of sessions', () => {
+    service.all().subscribe(sessions => {
+      expect(sessions).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should return a session', () => {
+    service.detail('1').subscribe(session => {
+      expect(session).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should delete a session', () => {
+    service.delete('1').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should create a session', () => {
+    service.create({
+      teacher_id: 1,
+      users: [],
+      name: 'Session test', date: new Date(), description: 'Description test'
+    }).subscribe(session => {
+      expect(session).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should update a session', () => {
+    service.update('1', {
+      teacher_id: 1,
+      users: [],
+      name: 'Session test',
+      date: new Date(), description: 'Description test updated'
+    }).subscribe(session => {
+      expect(session).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should participate in a session', () => {
+    service.participate('1', '1').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+  });
+
+  it('Integration test: should unparticipate in a session', () => {
+    service.unParticipate('1', '1').subscribe(response => {
+      expect(response).toBeTruthy();
+    });
   });
 });
--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
+import { BehaviorSubject, of } from 'rxjs';
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -13,5 +14,66 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('$isLogged()', () => {
+    it('should return an observable of the current logged in state', (done) => {
+      service.$isLogged().subscribe(isLoggedIn => {
+        expect(isLoggedIn).toBe(false); // Initially logged out
+        done();
+      });
+    });
+
+    it('should emit a new value when logged in state changes', (done) => {
+      const mockSessionInfo: SessionInformation = { token: 'test', type: 'test', id: 1, username: 'test', firstName: 'test', lastName: 'test', admin: false };
+
+      service.$isLogged().subscribe(isLoggedIn => {
+        if (isLoggedIn) {
+          expect(isLoggedIn).toBe(true);
+          done();
+        }
+      });
+
+      service.logIn(mockSessionInfo);
+    });
+  });
+
+  describe('logIn()', () => {
+    it('should update session information and logged in state', () => {
+      const mockSessionInfo: SessionInformation = { token: 'test', type: 'test', id: 1, username: 'test', firstName: 'test', lastName: 'test', admin: false };
+
+      service.logIn(mockSessionInfo);
+
+      expect(service.sessionInformation).toEqual(mockSessionInfo);
+      expect(service.isLogged).toBe(true);
+    });
+
+    it('should emit a new value on the isLoggedSubject', () => {
+      const spy = jest.spyOn(service.isLoggedSubject, 'next');
+      const mockSessionInfo: SessionInformation = { token: 'test', type: 'test', id: 1, username: 'test', firstName: 'test', lastName: 'test', admin: false };
+
+      service.logIn(mockSessionInfo);
+
+      expect(spy).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('logOut()', () => {
+    it('should clear session information and set logged in state to false', () => {
+      const mockSessionInfo: SessionInformation = { token: 'test', type: 'test', id: 1, username: 'test', firstName: 'test', lastName: 'test', admin: false };
+      service.logIn(mockSessionInfo);
+
+      service.logOut();
+
+      expect(service.sessionInformation).toBeUndefined();
+      expect(service.isLogged).toBe(false);
+    });
+
+    it('should emit a new value on the isLoggedSubject', () => {
+      const spy = jest.spyOn(service.isLoggedSubject, 'next');
+      service.logOut();
+
+      expect(spy).toHaveBeenCalledWith(false);
+    });
   });
 });
